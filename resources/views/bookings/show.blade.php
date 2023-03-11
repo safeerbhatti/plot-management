004d76376120a990f8f8927c91cc5b075e3a6566
Muhammad Samo <bldstrikr@gmail.com>
2023-03-12 01:46:35 +0500

    updated

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -1,67 +1,53 @@
 @extends('layouts.app', ['slug' => $slug]) @section('content')
 
 <div class="container-fluid">
-    <div class="card border-left-primary shadow my-5">
-        <h3 class="card-header font-weight-bold text-primary">Booking Details</h3>
-
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Booking ID: {{ $booking->id }}</li>
-            <li class="list-group-item">Class: {{ $booking->plot->class}}</li>
-            <li class="list-group-item">Number: {{ $booking->plot->plot_number}} </li>
-            <li class="list-group-item">Total Area in Square feet: {{ $booking->plot->plot_area_in_square_feet}}</li>
-        </ul>
-    </div>
-
-    <div class="card border-left-success shadow my-5">
-        <h4 class="card-header font-weight-bold text-success">Pricing</h4>
-
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Price/Square feet: Rupees {{ $booking->price_square_feet }}/-</li>
-            <li class="list-group-item">Total Amount: Rupees {{ $booking->total_amount }}/-</li>
-        </ul>
-    </div>
-
-    <div class="card border-left-danger shadow my-5">
-        <h4 class="card-header font-weight-bold text-danger">Development Charges</h4>
-
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Amount: Rupees {{ $booking->development_charges }}/-</li>
-        </ul>
-    </div>
-
-    <div class="card border-left-info shadow my-5">
-        <h4 class="card-header font-weight-bold text-info">Development Charges</h4>
-
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Amount: Rupees {{$booking->bi_yearly_fee}}</li>
-            <li class="list-group-item">
-                @if ($booking->bi_yearly_type === 'monthly')
-                <p>Bi Yearly Fee added in instalments.</p>
-                @elseif($booking->bi_yearly_type === 'once')
-                <p>Bi yearly fee saperate from monthly instalments.</p>
-                @endif
-            </li>
-        </ul>
-    </div>
-
-    <div class="card border-left-warning shadow my-5">
-        <h4 class="card-header font-weight-bold text-warning">Payment Information</h4>
-
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Down Payment: Rupees {{ $booking->down_payment}}/-</li>
-            <li class="list-group-item"> Instalment/Month: Rupees {{ $booking->instalment_per_month}}/-</li>
-            <li class="list-group-item"> Remaining Amount: Rupees {{ $booking->remaining_amount }}/-</li>
-            <li class="list-group-item"> Total Instalment Duration: {{ $booking->instalment_duration }} months</li>
-            <li class="list-group-item"> Remaining Instalments Duration: {{ $booking->remaining_duration }} months</li>
-        </ul>
-    </div>
 
     <ul class="list-group list-group-flush">
-        <li class="list-group-item"><a href="{{$booking->agreement_file}}">Click here </a>to view Agreement</li>
-        <li class="list-group-item"><a href="/{{$slug}}/invoices/{{$booking->id}}">Click here</a> to check paid invoices</li>
-        <li class="list-group-item"><a href="/{{$slug}}/invoice/pay/{{$booking->id}}">Click here</a> to pay Instalment</li>
-        <li class="list-group-item"> Remaining Instalments Duration: {{ $booking->remaining_duration }} months</li>
-        <li class="list-group-item"> @if($booking->bi_fee_status === 'unpaid')
+        <li>Plot: {{ $booking->plot->plot_number}} </li>
+        <li>Size: {{ $booking->plot->plot_area_in_square_feet}}</li>
+        <li>Scheme Name: {{ $scheme->name}}</li>
+        <li>Total Amount: Rupees {{ $booking->total_amount }}/-</li>
+        <li>
+            @if ($booking->bi_yearly_type === 'monthly')
+            <p>Bi Yearly Fee added in instalments.</p>
+            @elseif($booking->bi_yearly_type === 'once')
+            <p>Bi yearly fee saperate from monthly instalments.</p>
+            @endif
+        </li>
+        <li>Development Charges: {{ $booking->development_charges }}/-</li>
+        <li>Booking ID: {{ $booking->id }}</li>
+        <li>Class: {{ $booking->plot->class}}</li>
+    </ul>
+
+    <h4 class="card-header font-weight-bold text-success">Pricing</h4>
+
+    <ul class="list-group list-group-flush">
+        <li>Price/Square feet: Rupees {{ $booking->price_square_feet }}/-</li>
+    </ul>
+
+
+    <h4 class="card-header font-weight-bold text-info">Development Charges</h4>
+
+    <ul class="list-group list-group-flush">
+        <li>Amount: Rupees {{$booking->bi_yearly_fee}}</li>
+    </ul>
+
+    <h4 class="card-header font-weight-bold text-warning">Payment Information</h4>
+
+    <ul class="list-group list-group-flush">
+        <li>Down Payment: Rupees {{ $booking->down_payment}}/-</li>
+        <li> Instalment/Month: Rupees {{ $booking->instalment_per_month}}/-</li>
+        <li> Remaining Amount: Rupees {{ $booking->remaining_amount }}/-</li>
+        <li> Total Instalment Duration: {{ $booking->instalment_duration }} months</li>
+        <li> Remaining Instalments Duration: {{ $booking->remaining_duration }} months</li>
+    </ul>
+
+    <ul class="list-group list-group-flush">
+        <li><a href="{{$booking->agreement_file}}">Click here </a>to view Agreement</li>
+        <li><a href="/{{$slug}}/invoices/{{$booking->id}}">Click here</a> to check paid invoices</li>
+        <li><a href="/{{$slug}}/invoice/pay/{{$booking->id}}">Click here</a> to pay Instalment</li>
+        <li> Remaining Instalments Duration: {{ $booking->remaining_duration }} months</li>
+        <li> @if($booking->bi_fee_status === 'unpaid')
             <a href="/bi-yearly/pay/{{$booking->id}}">Click here</a> to pay Bi Yearly Fee.
             @elseif($booking->bi_fee_status === 'half')
             Bi Yearly Fee partially paid, remaining amount added to dues.
@@ -69,7 +55,7 @@
             Bi Yearly Fee already paid.
             @endif
         </li>
-        <li class="list-group-item"> @if($booking->dev_charges_status === 'unpaid')
+        <li> @if($booking->dev_charges_status === 'unpaid')
             <a href="/dev-charges/pay/{{$booking->id}}">Click here</a> to pay Development Charges.
             @elseif($booking->dev_charges_status === 'half')
             Development charges partially paid, remaining amount added to dues.
@@ -77,28 +63,19 @@
             Development Charges paid.
             @endif
         </li>
-        <li class="list-group-item"> <a href="/{{$slug}}/assign/customer/{{$booking->id}}">Click here</a> to assign a Customer</li>
-        <li class="list-group-item"> Remaining Instalments Duration: {{ $booking->remaining_duration }} months</li>
+        <li> <a href="/{{$slug}}/assign/customer/{{$booking->id}}">Click here</a> to assign a Customer</li>
+        <li> Remaining Instalments Duration: {{ $booking->remaining_duration }} months</li>
     </ul>
 
-    <div class="card border-left-primary shadwo my-5">
-        <h4 class="card-header font-weight-bold text-primary">Customer(s) Information</h4>
-        @foreach($customers as $customer)
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Customer Name: {{$customer->name}}</li>
-            <li class="list-group-item"> Customer Phone: {{$customer->phone}}</li>
-            <li class="list-group-item"> Customer CNIC: {{$customer->cnic}}</li>
-            <li class="list-group-item">Customer Address: {{$customer->address}}</li>
-        </ul>
-        @endforeach
-    </div>
-
-
-
-
-
-
-
+    <h4 class="card-header font-weight-bold text-primary">Customer(s) Information</h4>
+    @foreach($customers as $customer)
+    <ul class="list-group list-group-flush">
+        <li>Customer Name: {{$customer->name}}</li>
+        <li> Customer Phone: {{$customer->phone}}</li>
+        <li> Customer CNIC: {{$customer->cnic}}</li>
+        <li>Customer Address: {{$customer->address}}</li>
+    </ul>
+    @endforeach
 
 </div>
 
--- resources/views/schemes/index.blade.php
+++ resources/views/schemes/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app') @section('content')
 
 <div class="container-fluid">
-    <div class="schemes-container flex-wrap d-flex justify-content-start">
+    <div class="schemes-container w-100 flex-wrap d-flex justify-content-start">
         @foreach($schemes as $scheme)
-        <div class="card border-top-primary w-25 shadow mx-3">
-            <h1 class="card-header text-primary font-weight-bold text-center">{{ $scheme->name }}</h1>
+        <div class="card border-top-primary shadow">
+            <h1 class="card-header text-primary font-weight-bolder text-center">{{ $scheme->name }}</h1>
 
-            <div class="card-body card-text font-weight-bold text-primary">
+            <div class="card-body card-text font-weight-bolder text-primary">
                 <div class="scheme-logo mx-auto w-50"><img class="rounded-circle img-fluid" src="http://plot-management.test/img/undraw_profile.svg" alt="scheme-logo"></div>
                 <div class="mt-5">Total plots </div>
                 <div class="my-1">Available plots:</div>
                 <div class="my-1">Booked plots:</div>
-                <div class="w-75 mx-auto mt-4"><button class="text-uppercase font-weight-bold btn text-primary text-center border rounded-pill w-100">select</button></div>
+                <div class="w-75 mx-auto mt-4"><a href="#" class="text-uppercase font-weight-bolder btn text-primary text-center border rounded-pill w-100">select</a></div>
             </div>
         </div>
         @endforeach
